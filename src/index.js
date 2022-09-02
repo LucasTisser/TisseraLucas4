1e0cc537c8d28cb29a2ad8131d6c9fb8d23515cc
LucasTisser <96512199+LucasTisser@users.noreply.github.com>
2022-09-02 18:01:45 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -5,28 +5,28 @@ app.use(express.json())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 const productos = Router()
-const listaProductos = [
+let listaProductos = [
 {
     "title":"Shampoo",
     "price":"5USD",
-    "id":"1"
+    "id":1
 },
 {
     "title":"Acondicionador",
     "price":"7USD",
-    "id":"2"
+    "id":2
 },
 {
     "title":"Jabon",
     "price":"2USD",
-    "id":"3"
+    "id":3
 }]
 productos.get('/', (req,res)=>{
     res.send(listaProductos)
 })
 productos.get('/:id', (req,res)=>{
     // Toma un producto segun su id
-    const id = req.params.id
+    const id = Number(req.params.id)
     const productoBuscado = listaProductos.filter(producto=>producto.id === id)
     if(listaProductos.length < id){
         res.status(400).send('No se ha encontrado un producto con dicho ID')
@@ -41,32 +41,33 @@ productos.post('/', (req, res,next) => {
     }
     next()
 },(req, res) => {
-    const {title ,price, id} = req.body
+    const {title ,price} = req.body
+    const id = Number(listaProductos[listaProductos.length-1].id) + 1 
     listaProductos.push({title,price,id})
     res.send('producto guardado con exito')} 
+
 )
 productos.put('/:id',(req,res)=>{
+    const id = Number(req.params.id)
     const {title,price} = req.body;
-    const productoId = listaProductos.filter(producto=>producto.id === Number(req.params.id))
-    if (productoId >= 0 ){
-        listaProductos[productoId] = {title,price}
-        listaProductos[productoId].id = Number(req.params.id)
-        res.send(listaProductos[productoId])
+    const index = listaProductos.findIndex(producto=>producto.id === id)
+    if (index >= 0 ){
+        listaProductos[index] = {title,price, id}
+        res.send(listaProductos[index])
     } else {
         res.status(404).send({error:"Producto no encontrado"})
     }
 })
 productos.delete('/:id',(req, res) => {
     // Elimina un producto segun su id
-    const id = req.params.id
+    const id = Number(req.params.id)
     // filtrar los datos para identificar el objeto a eliminar y eliminarlo
-    const productoBorrado = listaProductos.filter((elemento) => elemento.id !== id);
-    console.log(productoBorrado)
-    if (productoBorrado.length == listaProductos.length) {
+    let listaFiltrada = listaProductos.filter((elemento) => elemento.id !== id);
+    if (listaFiltrada.length == listaProductos.length) {
       res.status(404).send({error:"Producto no encontrado"})
     } else {
     // guardar el nuevo array con el nuevo objeto agregado
-    listaProductos.splice(productoBorrado, 1)
+    listaProductos = listaFiltrada 
     res.send("Elemento eliminado")
   }
 })
